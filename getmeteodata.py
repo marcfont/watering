cda368ef661ffdce9057e5958faf6e8b126af5d4
Marc Font <marc.font@gmail.com>
2019-05-30 18:14:24 +0200

    keep on coding

--- getmeteodata.py
+++ getmeteodata.py
@@ -2,7 +2,9 @@ import json
 import requests
 import urllib3
 import statistics
-from datetime import date
+from datetime import date, timedelta
+import math
+
 # no need to worry about SSL to verify connection to meteo.cat
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 HEADER = {'x-api-key': 'yTLyU2J2XraoSZ4LEHpG35izWgS22AMs1DmRJqmZ'}
@@ -28,7 +30,7 @@ def meteocat_api_request(api_date, param):
     try:
         # url model --> https://api.meteo.cat/xema/v1/variables/mesurades/36/2019/05/28?codiEstacio=CC
         url = 'https://api.meteo.cat/xema/v1/variables/mesurades/' + str(operations[param]) + '/' + str(api_date.year) \
-              + '/' + str(api_date.month).zfill(2) + '/'+str(api_date.day).zfill(2) + '?codiEstacio=CC'
+              + '/' + str(api_date.month).zfill(2) + '/' + str(api_date.day).zfill(2) + '?codiEstacio=CC'
         r = requests.get(url, headers=HEADER, verify=False)
 
         # sample:
@@ -40,8 +42,6 @@ def meteocat_api_request(api_date, param):
         if r.ok:
             data = json.loads(r.text)
             values = []
-
-            print(data)
 
             for d in data["lectures"]:
                 values.append(d["valor"])
@@ -67,5 +67,31 @@ def meteocat_api_request(api_date, param):
         print(ex)
 
 
+def evapotranspiration_rain_day(when):
+    [t_max, t_min] = meteocat_api_request(when, 'temp')
+    rain = meteocat_api_request(when, 'rain')
+    [rh_max, rh_min] = meteocat_api_request(when, 'rh')
+    rn_g = meteocat_api_request(when, 'rad')
+    u2 = meteocat_api_request(when, 'wind')
+
+    t_mean = (t_max + t_min) / 2
+    delta = (4098 * (0.6108 ** ((12.27 * t_mean) / (t_mean + 237.3)))) / ((t_mean + 237.3) ** 2)
+    y = 0.063   # directament de la taula 2.2 i sense fer servir la eq7
+    eq2 = delta / (delta + y * (1 + 0.34 * u2))
+    eq3 = y / (delta + y * (1 + 0.34 * u2))
+    eq4 = 900 / ((t_mean + 273) * u2)
+    ea = (1.431 * (rh_max / 100) + 2.564 * (rh_min / 100)) / 2
+    eot_max = 0.6108 * math.exp((17.27 * t_max) / (t_max + 237.3))
+    eot_min = 0.6108 * math.exp((17.27 * t_min) / (t_min + 237.3))
+    es = (eot_max + eot_min) / 2
+    es_ea = es - ea
+    eq5 = 0.408 * rn_g * eq2
+    eq6 = eq4 * es_ea * eq3
+    et0 = eq5 + eq6
+
+    return [et0, rain]
+
+
 if __name__ == '__main__':
-    print(meteocat_api_request(date.today(), 'rh'))
+    d = 2
+    print(evapotranspiration_rain_day(date.today()-timedelta(days=d)))
