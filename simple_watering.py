7d9ddd74f3aff547bfc5c8e2c4d7b7f2b3a521e7
Marc Font <marc.font@gmail.com>
2019-06-25 17:39:22 +0200

    keep on coding

--- simple_watering.py
+++ simple_watering.py
@@ -1,62 +1,80 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime, time, timedelta
-import threading
-import urllib3
+import logging
+import smtplib
+import ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import RPi.GPIO as GPIO
 
-# import RPi.GPIO as GPIO
 
-# no need to worry about SSL to verify connection to meteo.cat
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-HEADER = {'x-api-key': 'yTLyU2J2XraoSZ4LEHpG35izWgS22AMs1DmRJqmZ'}
+def send_email(subject, body):
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = "watering.espona@gmail.com"  # Enter your address
+    receiver_email = "marc.font@gmail.com"  # Enter receiver address
+    password = "BeWaterMyFriend"
 
-# GPIO.setmode(GPIO.BOARD)  #Board GPIO Pin numbers
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
+    message.attach(MIMEText(body, "plain"))
+    text = message.as_string()
 
-GPIO4_RIGHT = 16
-GPIO5_FAR = 18
-GPIO6_LEFT = 22
-
-CIRCUITS = [GPIO4_RIGHT, GPIO5_FAR, GPIO6_LEFT]
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, text)
 
 
 def enable_valve(valve_id):
-    print('enable thread id: ' + str(threading.get_ident()))
-    print("Enable valve num: " + str(valve_id))
-    print("Now watering........")
+    logging.info("Enable valve in pin: " + str(valve_id))
+    logging.info("Now watering........")
+    send_email("Enable", str(CIRCUIT_NAMES[valve_id]) + " has been enabled.")
 
-    # GPIO.output(valve_id, GPIO.HIGH)
+    GPIO.output(valve_id, GPIO.HIGH)
 
 
 def disable_valve(valve_id):
-    print('disable thread id: ' + str(threading.get_ident()))
     print("..............and watering stops")
-    print("Disable valve num: " + str(valve_id))
+    print("Disable valve in pin: " + str(valve_id))
+    send_email("Disable", str(CIRCUIT_NAMES[valve_id]) + " has been disabled.")
 
-    # GPIO.output(valve_id, GPIO.LOW)
-
-    global result_available
-    result_available.set()
+    GPIO.output(valve_id, GPIO.LOW)
 
 
-# def gpio_init():
-#     GPIO.setmode(GPIO.BOARD)
-#
-#     GPIO.setup(GPIO2_FLOW_METER, GPIO.IN)
-#     GPIO.setup(GPIO4_RIGHT, GPIO.OUT)
-#     GPIO.setup(GPIO5_FAR, GPIO.OUT)
-#     GPIO.setup(GPIO6_LEFT, GPIO.OUT)
-#
-#     # this shouldn't be needed but here it comes just in case
-#     GPIO.output(GPIO4_RIGHT, GPIO.LOW)
-#     GPIO.output(GPIO5_FAR, GPIO.LOW)
-#     GPIO.output(GPIO6_LEFT, GPIO.LOW)
+def gpio_init():
+    GPIO.setmode(GPIO.BOARD)
+
+    GPIO.setup(GPIO_4_RIGHT, GPIO.OUT)
+    GPIO.setup(GPIO_5_FAR, GPIO.OUT)
+    GPIO.setup(GPIO_6_LEFT, GPIO.OUT)
+
+    # this shouldn't be needed but here it comes just in case
+    GPIO.output(GPIO_4_RIGHT, GPIO.LOW)
+    GPIO.output(GPIO_5_FAR, GPIO.LOW)
+    GPIO.output(GPIO_6_LEFT, GPIO.LOW)
 
 
 if __name__ == '__main__':
-    # gpio_init()
+    logging.basicConfig(filename='simple_watering.log', level=logging.INFO)
+    logging.info('---------------------------------------------')
+    logging.info('---------------system boot-------------------')
+    logging.info('---------------------------------------------')
 
     background_scheduler = BackgroundScheduler()
     background_scheduler.start()
     not_scheduled = True
+
+    GPIO_4_RIGHT = 16
+    GPIO_5_FAR = 18
+    GPIO_6_LEFT = 22
+
+    CIRCUITS = [GPIO_4_RIGHT, GPIO_5_FAR, GPIO_6_LEFT]
+    CIRCUIT_NAMES = dict([(GPIO_4_RIGHT, 'Right circuit'), (GPIO_5_FAR, 'Far circuit'), (GPIO_6_LEFT, 'Right circuit')])
+
+    gpio_init()
 
     START_TIME_MORNING = time(6, 0, 0)
     START_TIME_NIGHT = time(22, 0, 0)
@@ -88,7 +106,7 @@ if __name__ == '__main__':
         background_scheduler.add_job(disable_valve, 'cron', hour=night_run.hour, minute=night_run.minute,
                                      second=night_run.second, max_instances=1, args=[CIRCUITS[i]])
 
-    print(background_scheduler.print_jobs())
+    logging.info(background_scheduler.print_jobs())
 
     while True:
         pass
