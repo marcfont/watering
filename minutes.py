7058826c4a18c749ecccf2c4836082100e73748e
Marc Font <marc.font@gmail.com>
2019-07-11 13:20:01 +0200

    keep on coding

--- minutes.py
+++ minutes.py
@@ -77,30 +77,30 @@ def print_evapotranspiration_rain_particular_days():
 
     when = date.today()
 
-    for j in range(1, 10):
-        when.replace(2019, 7, j)
-        [t_max, t_min] = __meteocat_api_request(when, 'temp')
-        rain = __meteocat_api_request(when, 'rain')
-        [rh_max, rh_min] = __meteocat_api_request(when, 'rh')
-        rn_g = __meteocat_api_request(when, 'rad')
-        u2 = __meteocat_api_request(when, 'wind')
+    #for j in range(, 11):
+    when = when.replace(2019, 7, 10)
+    [t_max, t_min] = __meteocat_api_request(when, 'temp')
+    rain = __meteocat_api_request(when, 'rain')
+    [rh_max, rh_min] = __meteocat_api_request(when, 'rh')
+    rn_g = __meteocat_api_request(when, 'rad')
+    u2 = __meteocat_api_request(when, 'wind')
 
-        t_mean = (t_max + t_min) / 2
-        delta = (4098 * (0.6108 ** ((12.27 * t_mean) / (t_mean + 237.3)))) / ((t_mean + 237.3) ** 2)
-        y = 0.063   # directament de la taula 2.2 i sense fer servir la eq7
-        eq2 = delta / (delta + y * (1 + 0.34 * u2))
-        eq3 = y / (delta + y * (1 + 0.34 * u2))
-        eq4 = 900 / ((t_mean + 273) * u2)
-        ea = (1.431 * (rh_max / 100) + 2.564 * (rh_min / 100)) / 2
-        eot_max = 0.6108 * math.exp((17.27 * t_max) / (t_max + 237.3))
-        eot_min = 0.6108 * math.exp((17.27 * t_min) / (t_min + 237.3))
-        es = (eot_max + eot_min) / 2
-        es_ea = es - ea
-        eq5 = 0.408 * rn_g * eq2
-        eq6 = eq4 * es_ea * eq3
-        et0 = eq5 + eq6
+    t_mean = (t_max + t_min) / 2
+    delta = (4098 * (0.6108 ** ((12.27 * t_mean) / (t_mean + 237.3)))) / ((t_mean + 237.3) ** 2)
+    y = 0.063   # directament de la taula 2.2 i sense fer servir la eq7
+    eq2 = delta / (delta + y * (1 + 0.34 * u2))
+    eq3 = y / (delta + y * (1 + 0.34 * u2))
+    eq4 = 900 / ((t_mean + 273) * u2)
+    ea = (1.431 * (rh_max / 100) + 2.564 * (rh_min / 100)) / 2
+    eot_max = 0.6108 * math.exp((17.27 * t_max) / (t_max + 237.3))
+    eot_min = 0.6108 * math.exp((17.27 * t_min) / (t_min + 237.3))
+    es = (eot_max + eot_min) / 2
+    es_ea = es - ea
+    eq5 = 0.408 * rn_g * eq2
+    eq6 = eq4 * es_ea * eq3
+    et0 = eq5 + eq6
 
-        print(str(when), et0, rain)
+    print(str(when), et0, rain)
 
 
 def evapotranspiration_rain_day(start_day, num_days):
@@ -175,4 +175,5 @@ def minutes(start_day, num_days):
 
 
 if __name__ == '__main__':
-    print(evapotranspiration_rain_day(0, 1))
+    # print(evapotranspiration_rain_day(0, 1))
+    print_evapotranspiration_rain_particular_days()
---
+++ sched_test.py
@@ -0,0 +1,75 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime, time, timedelta
+
+START_TIME_MORNING = time(13, 22, 0)
+START_TIME_NIGHT = time(13, 24, 0)
+
+START_TIME_MORNING_2 = time(13, 26, 0)
+START_TIME_NIGHT_2 = time(13, 28, 0)
+
+DELAY_BETWEEN_CIRCUITS = 3
+
+
+def enable_valve():
+    print('running enable_valve')
+
+
+def disable_valve():
+    print('running disable_valve')
+
+
+def schedule_morning_run():
+    print('running schedule_morning_run')
+
+    seconds_first = [5, 5, 5]
+
+    run_time = datetime.now()
+
+    for i in range(1, 4):
+        run_time = run_time + timedelta(seconds=DELAY_BETWEEN_CIRCUITS)
+        background_scheduler.add_job(enable_valve, 'date', run_date=run_time)
+
+        run_time = run_time + timedelta(seconds=DELAY_BETWEEN_CIRCUITS + seconds_first[i])
+        background_scheduler.add_job(disable_valve, 'date', run_date=run_time)
+
+
+def schedule_night_run():
+    print('running schedule_night_run')
+
+    seconds_second = [7, 7, 7]
+
+    run_time = datetime.now()
+
+    for i in range(1, 4):
+        run_time = run_time + timedelta(seconds=DELAY_BETWEEN_CIRCUITS)
+        background_scheduler.add_job(enable_valve, 'date', run_date=run_time)
+
+        run_time = run_time + timedelta(seconds=DELAY_BETWEEN_CIRCUITS + seconds_second)
+        background_scheduler.add_job(disable_valve, 'date', run_date=run_time)
+
+
+if __name__ == '__main__':
+    background_scheduler = BackgroundScheduler()
+    background_scheduler.start()
+
+    morning_run = datetime.now().replace(hour=START_TIME_MORNING.hour, minute=START_TIME_MORNING.minute,
+                                         second=START_TIME_MORNING.second)
+    night_run = datetime.now().replace(hour=START_TIME_NIGHT.hour, minute=START_TIME_NIGHT.minute,
+                                       second=START_TIME_NIGHT.second)
+
+    morning_run_2 = datetime.now().replace(hour=START_TIME_MORNING_2.hour, minute=START_TIME_MORNING_2.minute,
+                                           second=START_TIME_MORNING_2.second)
+    night_run_2 = datetime.now().replace(hour=START_TIME_NIGHT.hour, minute=START_TIME_NIGHT.minute,
+                                         second=START_TIME_NIGHT.second)
+
+    background_scheduler.add_job(schedule_morning_run, 'cron', hour=morning_run.hour, minute=morning_run.minute,
+                                 second=morning_run.second, max_instances=1)
+    background_scheduler.add_job(schedule_night_run, 'cron', hour=night_run.hour, minute=night_run.minute,
+                                 second=night_run.second, max_instances=1)
+    background_scheduler.add_job(schedule_morning_run, 'cron', hour=morning_run_2.hour, minute=morning_run_2.minute,
+                                 second=morning_run_2.second, max_instances=1)
+    background_scheduler.add_job(schedule_night_run, 'cron', hour=night_run_2.hour, minute=night_run_2.minute,
+                                 second=night_run_2.second, max_instances=1)
+
+    while True:
+        time.sleep(1000)
