82b1ad3250d3eadc608d273586a4122ef0f37c1a
Marc Font <marc.font@gmail.com>
2020-03-10 12:21:19 +0100

    Update manual_run.py

--- manual_run.py
+++ manual_run.py
@@ -5,26 +5,26 @@ MINUTES = [1, 1, 1]
 DELAY_BETWEEN_CIRCUITS = 5
 
 def enable_valve():
-    print('running enable_valve')
+	print('running enable_valve')
 
 
 def disable_valve():
-    print('running disable_valve')
+	print('running disable_valve')
 
 
 if __name__ == '__main__':
-    background_scheduler = BackgroundScheduler()
-    background_scheduler.start()
-	
+	background_scheduler = BackgroundScheduler()
+	background_scheduler.start()
+
 	run_time = datetime.now()
 	
-    for i in range(0, len(MINUTES)):
-        run_time = run_time + timedelta(seconds=DELAY_BETWEEN_CIRCUITS)
-        background_scheduler.add_job(enable_valve, 'date', run_date=run_time)
+	for i in range(0, len(MINUTES)):
+		run_time = run_time + timedelta(seconds=DELAY_BETWEEN_CIRCUITS)
+		background_scheduler.add_job(enable_valve, 'date', run_date=run_time)
 
-        run_time = run_time + timedelta(seconds=DELAY_BETWEEN_CIRCUITS + MINUTES[i] * 60)
-        background_scheduler.add_job(disable_valve, 'date', run_date=run_time)	
+		run_time = run_time + timedelta(seconds=DELAY_BETWEEN_CIRCUITS + MINUTES[i] * 60)
+		background_scheduler.add_job(disable_valve, 'date', run_date=run_time)	
 	
     
 	while True:
-        pass
+		pass
