ffdf7abd8cfc2479819a0bb581aba0c60c8657d1
Marc Font <marc.font@gmail.com>
2020-03-21 16:22:22 +0100

    Update valve_scheduler.py

--- valve_scheduler.py
+++ valve_scheduler.py
@@ -375,6 +375,10 @@ if __name__ == '__main__':
         logging.basicConfig(filename='watering.log', level=logging.INFO)
         logging.info('------------------------------------------------------------')
         logging.info('------------------------System boot on: ' + datetime.now().strftime('%d/%m/%Y, %H:%M:%S'))
+        if MANUAL_MINUTES:
+            minutes_morning = MINUTES
+			logging.info('------------------------MANUAL_MINUTES: ' + str(MANUAL_MINUTES))
+			logging.info('------------------------MINUTES: ' + str(MINUTES))
         logging.info('------------------------MORNING_RUN_ENABLED: ' + str(MORNING_RUN_ENABLED))
         logging.info('------------------------START_TIME_MORNING: ' + str(START_TIME_MORNING))
         logging.info('------------------------NIGHT_RUN_ENABLED: ' + str(NIGHT_RUN_ENABLED))
