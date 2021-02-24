1b420fc284b0a40aab540e56e891f4268abe0721
Marc Font <marc.font@gmail.com>
2021-02-24 14:30:57 +0100

    new version

--- valve_scheduler.py
+++ valve_scheduler.py
@@ -27,16 +27,12 @@ HEADER = {'x-api-key': 'yTLyU2J2XraoSZ4LEHpG35izWgS22AMs1DmRJqmZ'}
 MANUAL_MINUTES = True
 MINUTES = [10, 4, 10]
 
-MORNING_RUN_ENABLED = True
-NIGHT_RUN_ENABLED = False
-
 GPIO_2_FLOW_METER = 13
 GPIO_4_RIGHT = 16
 GPIO_5_FAR = 18
 GPIO_6_LEFT = 22
 
 START_TIME_MORNING = time(6, 0, 0)
-START_TIME_NIGHT = time(22, 0, 0)
 
 # ------------------ HARDWARE PARAMETERS ------------------
 
@@ -141,85 +137,24 @@ def __meteocat_api_request(api_date, operation_id):
 		send_email('General failure', 'Error in __meteocat_api_request: ' + repr(ex))
 		
 
-def evapotranspiration_rain_day(morning_night):
-	"""
-	evapotranspiration calculation using meteo.cat api queries and FAO formula (see readme)
-	:param morning_night: if 'morning' returns data from yesterday + rain from today. Returns data from today otherwise
-	:return: [evapotranspiration, rain (mm)]
-	"""
-
+def minutes():
 	try:
-		et0_out = rain_out = 0
-
-		if morning_night == 'morning':
-			# et0 from yesterday, rain from yesterday + today
-			when = date.today()
-			rain = __meteocat_api_request(when, 'rain')
-			when = date.today() - timedelta(days=1)
-			rain = rain + __meteocat_api_request(when, 'rain')
-		elif morning_night == 'night':
-			# et0 from today, rain from today
-			when = date.today()
-			rain = __meteocat_api_request(when, 'rain')
-		else:
-			logging.error(datetime.now().strftime('%d/%m/%Y, %H:%M:%S') + ' Mongo error in evapotranspiration_rain_day')
-			send_email('General failure', 'Mongo error in evapotranspiration_rain_day')
-
-		[t_max, t_min] = __meteocat_api_request(when, 'temp')
-		[rh_max, rh_min] = __meteocat_api_request(when, 'rh')
-		rn_g = __meteocat_api_request(when, 'rad')
-		u2 = __meteocat_api_request(when, 'wind')
-
-		t_mean = (t_max + t_min) / 2
-		delta = (4098 * (0.6108 ** ((12.27 * t_mean) / (t_mean + 237.3)))) / ((t_mean + 237.3) ** 2)
-		y = 0.063   # directament de la taula 2.2 i sense fer servir la eq7
-		eq2 = delta / (delta + y * (1 + 0.34 * u2))
-		eq3 = y / (delta + y * (1 + 0.34 * u2))
-		eq4 = 900 / ((t_mean + 273) * u2)
-		ea = (1.431 * (rh_max / 100) + 2.564 * (rh_min / 100)) / 2
-		eot_max = 0.6108 * math.exp((17.27 * t_max) / (t_max + 237.3))
-		eot_min = 0.6108 * math.exp((17.27 * t_min) / (t_min + 237.3))
-		es = (eot_max + eot_min) / 2
-		es_ea = es - ea
-		eq5 = 0.408 * rn_g * eq2
-		eq6 = eq4 * es_ea * eq3
-		et0 = eq5 + eq6
-
-		et0_out = et0_out + et0
-		rain_out = rain_out + rain
-
-		return [round(et0_out, 1), round(rain_out, 1)]
-
-	except Exception as ex:
-		logging.error(datetime.now().strftime('%d/%m/%Y, %H:%M:%S') + ' Error in evapotranspiration_rain_day: ' + repr(ex))
-		send_email('General failure', 'Error in evapotranspiration_rain_day: ' + repr(ex))
-
-
-def minutes(morning_night):
-	try:
-		KJ = 0.6
+		KJ = 0.5
 		EFFECTIVE_RAIN = 0.8
 		REAL_ETO_TO_MINUTES_SLOPE = 13
 		STRAWBERRY_TO_GRASS = 1/8
-		NIGHT_PORTION = 1/5
-		MORNING_PORTION = 4/5
-		MIN_ETO_REAL = 0.6 # Això són 8.4 minuts de gespa i 1.1 de maduixes
+		MIN_ETO_REAL = 0.5 # Això són 8.4 minuts de gespa i 1.1 de maduixes
 
-		[eto, rain] = evapotranspiration_rain_day(morning_night)
+		[eto, rain] = __evapo_yesterday_Oris()
 		eto_real = eto * KJ - rain * EFFECTIVE_RAIN
 
 		if eto_real >= MIN_ETO_REAL:
 			grass_minutes = eto_real * REAL_ETO_TO_MINUTES_SLOPE
 			strawberry_minutes = grass_minutes * STRAWBERRY_TO_GRASS
-
-			if morning_night == 'morning':
-				morning = [round(grass_minutes * MORNING_PORTION / 2), round(strawberry_minutes * MORNING_PORTION),
-						   round(grass_minutes * MORNING_PORTION / 2)]
-				return morning
-			elif morning_night == 'night':
-				night = [round(grass_minutes * NIGHT_PORTION / 2), round(strawberry_minutes * NIGHT_PORTION),
-						 round(grass_minutes * NIGHT_PORTION / 2)]
-				return night
+			# todo: review morning portion
+			morning = [round(grass_minutes / 2), round(strawberry_minutes), round(grass_minutes / 2)]
+			return morning
+	
 			else:
 				logging.error(datetime.now().strftime('%d/%m/%Y, %H:%M:%S') + ' Mongo error in minutes')
 				send_email('General failure', 'Mongo error in minutes')
@@ -232,8 +167,7 @@ def minutes(morning_night):
 
 
 def print_minutes():
-	print('Morning: ' + str(minutes('morning')))
-	print('Night: ' + str(minutes('night')))
+	print('Morning: ' + str(minutes()))
 	
 def send_email(subject, body):
 	try:
@@ -332,58 +266,30 @@ def schedule_morning_run():
 	try:
 		# Morning run takes into account today and yesterday
 		if MANUAL_MINUTES:
-			minutes_morning = MINUTES
+			minutes = MINUTES
 		else:
-			minutes_morning = minutes('morning')
+			minutes = minutes()
 
 		send_email('Watering morning running: ',
 				   'START_TIME_MORNING: ' + datetime.now().strftime('%H:%M:%S') + ' \n' +
-				   'MINUTES_MORNING: ' + str(minutes_morning))
+				   'MINUTES_MORNING: ' + str(minutes))
 		logging.info(datetime.now().strftime('%d/%m/%Y, %H:%M:%S') + ' Watering morning running: \n' +
 					 'START_TIME_MORNING: ' + datetime.now().strftime('%H:%M:%S') + ' \n' +
-					 'MINUTES_MORNING: ' + str(minutes_morning))
+					 'MINUTES_MORNING: ' + str(minutes))
 
 		run_time = datetime.now()
-		if minutes_morning != [0, 0, 0]:
+		if minutes != [0, 0, 0]:
 			for i in range(0, len(CIRCUITS)):
 				run_time = run_time + timedelta(seconds=DELAY_BETWEEN_CIRCUITS)
 				background_scheduler.add_job(enable_valve, 'date', run_date=run_time, args=[CIRCUITS[i]])
 
-				run_time = run_time + timedelta(minutes=minutes_morning[i], seconds=DELAY_BETWEEN_CIRCUITS)
+				run_time = run_time + timedelta(minutes=minutes[i], seconds=DELAY_BETWEEN_CIRCUITS)
 				background_scheduler.add_job(disable_valve, 'date', run_date=run_time, args=[CIRCUITS[i]])
 
 	except Exception as ex:
 		logging.error(datetime.now().strftime('%d/%m/%Y, %H:%M:%S') + ' Error in schedule_morning_run: ' + repr(ex))
 		send_email('General failure', 'Error in schedule_morning_run: ' + repr(ex))
 
-
-def schedule_night_run():
-	try:
-		# Night run takes into account just today
-		if MANUAL_MINUTES:
-			minutes_night = MINUTES
-		else:
-			minutes_night = minutes('night')        
-
-		send_email('Watering night running: ',
-				   'START_TIME_NIGHT: ' + datetime.now().strftime('%H:%M:%S') + ' \n' +
-				   'MINUTES_NIGHT: ' + str(minutes_night))
-		logging.info(datetime.now().strftime('%d/%m/%Y, %H:%M:%S') + ' Watering night running: \n' +
-					 'START_TIME_NIGHT: ' + datetime.now().strftime('%H:%M:%S') + ' \n' +
-					 'MINUTES_NIGHT: ' + str(minutes_night))
-
-		run_time = datetime.now()
-		if minutes_night != [0, 0, 0]:
-			for i in range(0, len(CIRCUITS)):
-				run_time = run_time + timedelta(seconds=DELAY_BETWEEN_CIRCUITS)
-				background_scheduler.add_job(enable_valve, 'date', run_date=run_time, args=[CIRCUITS[i]])
-
-				run_time = run_time + timedelta(minutes=minutes_night[i], seconds=DELAY_BETWEEN_CIRCUITS)
-				background_scheduler.add_job(disable_valve, 'date', run_date=run_time, args=[CIRCUITS[i]])
-
-	except Exception as ex:
-		logging.error(datetime.now().strftime('%d/%m/%Y, %H:%M:%S') + ' Error in schedule_night_run: ' + repr(ex))
-		send_email('General failure', 'Error in schedule_night_run: ' + repr(ex))
 
 def wait_for_network():
 	""" # wait until Internet connection comes up"""
@@ -409,7 +315,7 @@ def wait_for_network():
 	except Exception as ex:
 		logging.error(datetime.now().strftime('%d/%m/%Y, %H:%M:%S') + ' Error in wait_for_network: ' + repr(ex))
 		# this may fail anyway
-		send_email('General failure', 'Error in schedule_night_run: ' + repr(ex))
+		send_email('General failure', 'Error in wait_for_network: ' + repr(ex))
 
 
 if __name__ == '__main__':
@@ -417,10 +323,8 @@ if __name__ == '__main__':
 		logging.basicConfig(filename='watering.log', level=logging.INFO)
 		logging.info('------------------------------------------------------------')
 		logging.info('------------------------System boot on: ' + datetime.now().strftime('%d/%m/%Y, %H:%M:%S'))
-		logging.info('------------------------MORNING_RUN_ENABLED: ' + str(MORNING_RUN_ENABLED))
+		logging.info('------------------------MORNING_RUN_ENABLED: True')
 		logging.info('------------------------START_TIME_MORNING: ' + str(START_TIME_MORNING))
-		logging.info('------------------------NIGHT_RUN_ENABLED: ' + str(NIGHT_RUN_ENABLED))
-		logging.info('------------------------START_TIME_NIGHT: ' + str(START_TIME_NIGHT))
 		if MANUAL_MINUTES:
 			logging.info('------------------------MANUAL_MINUTES: ' + str(MANUAL_MINUTES))
 			logging.info('------------------------MINUTES: ' + str(MINUTES))		
@@ -442,21 +346,12 @@ if __name__ == '__main__':
 
 		morning_run = datetime.now().replace(hour=START_TIME_MORNING.hour, minute=START_TIME_MORNING.minute,
 											 second=START_TIME_MORNING.second)
-		night_run = datetime.now().replace(hour=START_TIME_NIGHT.hour, minute=START_TIME_NIGHT.minute,
-										   second=START_TIME_NIGHT.second)
 
-		if MORNING_RUN_ENABLED:
-			background_scheduler.add_job(schedule_morning_run, 'cron', hour=morning_run.hour, minute=morning_run.minute,
-										 second=morning_run.second)
-		if NIGHT_RUN_ENABLED:
-			background_scheduler.add_job(schedule_night_run, 'cron', hour=night_run.hour, minute=night_run.minute,
-
-										 second=night_run.second)
+		background_scheduler.add_job(schedule_morning_run, 'cron', hour=morning_run.hour, minute=morning_run.minute,
+									 second=morning_run.second)
 		
-		content = 'MORNING_RUN_ENABLED: ' + str(MORNING_RUN_ENABLED) + '\n' +\
-		'START_TIME_MORNING: ' + str(START_TIME_MORNING) + '\n' +\
-		'NIGHT_RUN_ENABLED: ' + str(NIGHT_RUN_ENABLED) + '\n' +\
-		'START_TIME_NIGHT: ' + str(START_TIME_NIGHT)
+		content = 'MORNING_RUN_ENABLED: True' + '\n' +\
+		'START_TIME_MORNING: ' + str(START_TIME_MORNING)
 
 		if MANUAL_MINUTES:
 			content = content + '\nMANUAL_MINUTES: ' + str(MANUAL_MINUTES) + '\n' +\
