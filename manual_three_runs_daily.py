505e8eca1a47a6affa77cdef39d32dc8c9d0de0d
Marc Font <marc.font@gmail.com>
2023-07-01 12:02:10 +0200

    local definition for the function

--- manual_three_runs_daily.py
+++ manual_three_runs_daily.py
@@ -1,16 +1,46 @@
 import sys
-from apscheduler.schedulers.background import BackgroundScheduler
 from valve_scheduler import *
 
 
-START_TIME_1 = t.strptime("11:54:00", '%H:%M:%S')
-START_TIME_2 = t.strptime("11:59:00", '%H:%M:%S')
-START_TIME_3 = t.strptime("12:05:00", '%H:%M:%S')
+START_TIME_1 = t.strptime("12:03:00", '%H:%M:%S')
+START_TIME_2 = t.strptime("12:08:00", '%H:%M:%S')
+START_TIME_3 = t.strptime("12:13:00", '%H:%M:%S')
 
 DELAY_BETWEEN_CIRCUITS = 5
 
-def self_kill():
-	sys.exit()
+def schedule_daily_run():
+	try:
+		minutes_to_run = [0, 0, 0]
+		if MANUAL_MINUTES:
+			minutes_to_run = []
+			for i in range(len(CIRCUIT_DEFINITIONS)):
+				minutes_to_run.append(int(CIRCUIT_DEFINITIONS[i]['MANUAL_MINUTES']))
+		else:
+			minutes_to_run = minutes()
+
+		send_email('Watering cycle running: \n',
+				   'START_TIME: ' + datetime.now().strftime('%H:%M:%S') + ' \n' +
+				   'CYCLE_MINUTES: ' + str(minutes_to_run))
+		logging.info(datetime.now().strftime('%d/%m/%Y, %H:%M:%S') + ' Watering cycle running: \n' +
+					 'START_TIME: ' + datetime.now().strftime('%H:%M:%S') + ' \n' +
+					 'CYCLE_MINUTES: ' + str(minutes_to_run))
+					 
+		#TODO guardar a la DB (veure TODO.txt)
+
+		run_time = datetime.now()
+		if minutes_to_run != [0, 0, 0]:
+			for i in range(0, len(CIRCUIT_DEFINITIONS)):
+				run_time = run_time + timedelta(seconds=DELAY_BETWEEN_CIRCUITS)
+				background_scheduler.add_job(enable_valve, 'date', run_date=run_time, 
+							args=[int(CIRCUIT_DEFINITIONS[i]['PORT']), CIRCUIT_DEFINITIONS[i]['NAME']])
+
+				run_time = run_time + timedelta(minutes=minutes_to_run[i], seconds=DELAY_BETWEEN_CIRCUITS)
+				background_scheduler.add_job(disable_valve, 'date', run_date=run_time, 
+							args=[int(CIRCUIT_DEFINITIONS[i]['PORT']), CIRCUIT_DEFINITIONS[i]['NAME']])
+
+	except Exception as ex:
+		logging.error(datetime.now().strftime('%d/%m/%Y, %H:%M:%S') + ' Error in schedule_daily_run: ' + traceback.print_exc())
+		send_email('General failure', 'Error in schedule_daily_run: ' + traceback.print_exc())
 
 if __name__ == '__main__':
 	background_scheduler = BackgroundScheduler()
